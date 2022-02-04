7a9f25de3aa7180f380032749f9ce62a0b36d549
kneeko97 <ncalsada@ucsd.edu>
2022-02-04 00:17:32 -0800

    lib + faulty test

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,1 +1,5 @@
-public class SkillDemo {}
+public class SkillDemo {
+    public static int multiply(int l, int w){
+        return l*w;
+    }
+}
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void area() {
+        assertEquals(11, SkillDemo.multiply(3, 4));
+    }
+}
